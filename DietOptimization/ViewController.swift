9e3eb366af7b90e8716f527aa6d391c0538f7eb9
randy876224691 <randy876224691@gmail.com>
2016-12-10 15:19:15 +1100

    create

---
+++ DietOptimization/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DietOptimization
+//
+//  Created by Randy-mac on 16/12/10.
+//  Copyright © 2016年 HD90+. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-DietOptimization/Pods-DietOptimization-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DietOptimization : NSObject
+@end
+@implementation PodsDummy_Pods_DietOptimization
+@end
